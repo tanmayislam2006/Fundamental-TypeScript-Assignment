48da5fed0eb0479d678cb0f69b2ec466d340f68a
tanmay2006 <tanmayislam101@gmail.com>
2025-11-20 20:40:29 +0600

    Add Person class to encapsulate name and age properties with a method to get details

--- solution.ts
+++ solution.ts
@@ -16,5 +16,15 @@ const getLength = (value: ArrayType) => {
     return value.length;
   }
 };
-console.log(getLength("typescript"));
-console.log(getLength([10, 20, 30, 40]));
+class Person {
+  name: string;
+  age: number;
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+  getDetails() {
+    return `'Name: ${this.name}, Age: ${this.age}'`;
+  }
+}
+
